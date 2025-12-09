7f2b8f2fa3dafdc4aa7d604824ea84b6f8f6eb8d
darizae <danielarizae@gmail.com>
2025-12-09 14:50:08 +0100

    Add tauri

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
+const host = process.env.TAURI_DEV_HOST;
+
 export default defineConfig({
     plugins: [react()],
+    clearScreen: false,
     server: {
-        port: 5173
-    }
+        port: 5173,
+        strictPort: true,
+        host: host || '127.0.0.1'
+    },
+    envPrefix: ['VITE_', 'TAURI_']
 });
